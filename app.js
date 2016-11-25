961cfac2ceaffdce9ba47804a64a97671a799ec2
Sergii Kaminskyi <sergii.kaminskyi@globalrelay.net>
2016-11-24 16:17:12 -0800

    redis

---
+++ app.js
@@ -0,0 +1,29 @@
+
+const WebSocketServer = require('ws').Server;
+const redis = require("redis"); // [1]
+const redisSub = redis.createClient();
+const redisPub = redis.createClient();
+
+//static file server
+const server = require('http').createServer(
+	require('ecstatic')({root: `${__dirname}/www`})
+);
+
+const wss = new WebSocketServer({server: server});
+
+wss.on('connection', ws => {
+	console.log('Client connected');
+	ws.on('message', msg => {
+		console.log(`Message: ${msg}`);
+		redisPub.publish('chat_messages', msg); // [2]
+	});
+});
+
+redisSub.subscribe('chat_messages'); // [3]
+redisSub.on('message', (channel, msg) => {
+	wss.clients.forEach((client) => {
+		client.send(msg);
+	});
+});
+
+server.listen(process.argv[2] || 8080);
